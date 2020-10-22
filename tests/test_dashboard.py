1dbebc4c31332a2bc16a8667580024fcab257fe6
Oege Dijk <oegedijk@gmail.com>
2020-10-22 08:56:47 +0200

    fixed dashboard tests

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -13,27 +13,43 @@ class DashboardTests(unittest.TestCase):
         X_train, y_train, X_test, y_test = titanic_survive()
         train_names, test_names = titanic_names()
 
-        model = RandomForestClassifier(n_estimators=5, max_depth=2)
-        model.fit(X_train, y_train)
+        self.model = RandomForestClassifier(n_estimators=5, max_depth=2)
+        self.model.fit(X_train, y_train)
 
         self.explainer = ClassifierExplainer(
-                            model, X_test, y_test, 
+                            self.model, X_test, y_test, 
                             cats=['Sex', 'Cabin', 'Embarked'],
                             labels=['Not survived', 'Survived'])
 
         self.dashboard = ExplainerDashboard(self.explainer, [ShapDependenceTab, "importances"])
 
+        self.pkl_dir = Path.cwd() / "tests" / "cli_assets" 
+        self.explainer.dump(self.pkl_dir / "explainer.joblib")
+        self.explainer.to_yaml(self.pkl_dir / "explainer.yaml")
+        self.dashboard.to_yaml(self.pkl_dir / "dashboard.yaml")
+
     def test_yaml(self):
         yaml = self.dashboard.to_yaml()
         self.assertIsInstance(yaml, str)
 
-class DashboardLoadTests(unittest.TestCase):
+    def test_yaml_dict(self):
+        yaml_dict = self.dashboard.to_yaml(return_dict=True)
+        self.assertIsInstance(yaml_dict, dict)
+        self.assertIn("dashboard", yaml_dict)
 
-    def test_load_config(self):
- 
+    def test_load_config_joblib(self):
         db = ExplainerDashboard.from_config(
-            Path.cwd() / "tests" / "cli_assets" / "explainer.joblib",
-            Path.cwd() / "tests" / "cli_assets" / "dashboard.yaml")
+            self.pkl_dir / "explainer.joblib",
+            self.pkl_dir / "dashboard.yaml")
+        self.assertIsInstance(db, ExplainerDashboard)
 
+    def test_load_config_yaml(self):
+        db = ExplainerDashboard.from_config(
+            self.pkl_dir / "dashboard.yaml")
+        self.assertIsInstance(db, ExplainerDashboard)
+
+    def test_load_config_explainer(self):
+        db = ExplainerDashboard.from_config(
+            self.explainer, self.pkl_dir / "dashboard.yaml")
         self.assertIsInstance(db, ExplainerDashboard)
         
