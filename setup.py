0b04fdac7b3980090be122de19f309a08cdafe15
Oege Dijk <oegedijk@gmail.com>
2020-12-01 15:43:03 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='explainerdashboard',
-    version='0.2.12.1',
+    version='0.2.13',
     description='explainerdashboard allows you quickly build an interactive dashboard to explain the inner workings of your machine learning model.',
     long_description="""
 
