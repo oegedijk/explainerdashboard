eaaddd791a35a8cba5a85528d4e5c047b850d172
Oege Dijk <oegedijk@gmail.com>
2021-01-09 13:25:22 +0100

    adds homegrown pdpbox

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,7 +59,7 @@ extensions = [
 
 autodoc_mock_imports = ['matplotlib', 'np', 'dash', 'dash_bootstrap_components',
                     'dash_html_components', 'dash_table', 'dash_core_components',
-                    'dtreeviz', 'numpy', 'pandas', 'pd', 'pdpbox', 
+                    'dtreeviz', 'numpy', 'pandas', 'pd', 
                     'sklearn', 'shap',  'plotly', 'shortuuid', 
                     'joblib', 'dash_auth', 'jupyter_dash', 'oyaml', 'click',
                     'flask', 'flask_simplelogin', 'werkzeug']
