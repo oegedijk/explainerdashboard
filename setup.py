690003fa0244925d25ef0f23e83fae6b32cab076
Oege Dijk <oegedijk@gmail.com>
2021-07-08 16:15:31 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='explainerdashboard',
-    version='0.3.5',
+    version='0.3.6.1',
     description='Quickly build Explainable AI dashboards that show the inner workings of so-called "blackbox" machine learning models.',
     long_description="""
 
