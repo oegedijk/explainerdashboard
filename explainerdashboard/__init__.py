96334237eeb00ce13a72a8de322338a2d94df4e8
Oege Dijk <oegedijk@gmail.com>
2022-12-31 21:46:56 +0100

    Update __init__.py

--- explainerdashboard/__init__.py
+++ explainerdashboard/__init__.py
@@ -1,5 +1,5 @@
 
-___version__ = "0.4.0"
+___version__ = "0.4.1"
 
 from .explainers import ClassifierExplainer, RegressionExplainer
 from .dashboards import ExplainerDashboard, ExplainerHub, InlineExplainer
