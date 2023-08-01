7f35c5b0d867258c5bd0723f200be21c604a5c95
Oege Dijk <oegedijk@gmail.com>
2023-08-01 10:24:14 +0200

    fixes hub cli test warning

--- tests/hub/test_hub_cli.py
+++ tests/hub/test_hub_cli.py
@@ -3,7 +3,7 @@ import pytest
 pytestmark = pytest.mark.cli
 
 def test_explainerhub_cli_help(explainer_hub_dump_folder, script_runner):
-    ret = script_runner.run('explainerhub', ' --help', cwd=str(explainer_hub_dump_folder))
+    ret = script_runner.run(['explainerhub', ' --help'], cwd=str(explainer_hub_dump_folder))
     assert ret.success
     assert ret.stderr == ''
 
