3a9374ff09ff2bef522e1eceea008abcc6a325cd
Alexander Zender <alexander.zender@h-da.de>
2023-08-03 09:18:14 +0200

    changed used dataset to titanic

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,17 +1,16 @@
-import unittest
-
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 from explainerdashboard import ClassifierExplainer, ExplainerDashboard
-from explainerdashboard.custom import ShapDependenceComposite
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 import os
+import numpy as np
 
 class CategoricalModelWrapper:
-    def __init__(self, model) -> None:
+    def __init__(self, model, categorical_label_test) -> None:
         self._model = model
+        self._categorical_label_test = categorical_label_test
         pass
 
     def _perform_one_hot_encoding(self, X, feature, values):
@@ -24,27 +23,21 @@ class CategoricalModelWrapper:
     
     def _perform_label_encoding(self, y):
         label_enc = LabelEncoder()
-        label_enc.fit([["unacc"],["acc"],["good"],["vgood"]])
+        label_enc.fit([["Survived"],["Not Survived"]])
         return pd.Series(label_enc.transform(y.values), name=y.name, index=y.index)
         
     def _perform_label_decoding(self, y):
         label_enc = LabelEncoder()
-        label_enc.fit([["unacc"],["acc"],["good"],["vgood"]])
+        label_enc.fit([["Survived"],["Not Survived"]])
         return pd.Series(label_enc.inverse_transform(y), name=y.name)
 
     def _preprocessor(self, X):
-        #Emulate a manual pipeline, e.g. what AutoML solutions can produce
-        #preprocess buying
-        X = self._perform_one_hot_encoding(X, "buying", [["vhigh"],["high"],["med"],["low"]])
-        X = self._perform_one_hot_encoding(X, "maint", [["vhigh"],["high"],["med"],["low"]])
-        X = self._perform_one_hot_encoding(X, "doors", [["2"],["3"],["4"],["5more"]])
-        X = self._perform_one_hot_encoding(X, "persons", [["2"],["4"],["more"]])
-        X = self._perform_one_hot_encoding(X, "lug_boot", [["small"],["med"],["big"]])
-        X = self._perform_one_hot_encoding(X, "safety", [["low"],["med"],["high"]])
-        return X
+        return X.drop(["Name"], axis=1)
 
     def _postprocessor(self, y):
-        return self._perform_label_decoding(y)
+        if self._categorical_label_test == True:
+            y = self._perform_label_decoding(y)
+        return y
 
     def predict(self, X):
         X = self._preprocessor(X)
@@ -56,30 +49,44 @@ class CategoricalModelWrapper:
         probabilities_raw = self._model.predict_proba(X)
         return probabilities_raw
 
-def generate_categorical_dataset_model_wrapper(categorical_label=False):
-    df = pd.read_csv(os.path.join(os.getcwd(), "tests\\test_assets\\car.csv"))
-    X_train, X_test, y_train, y_test = train_test_split(df.drop(["class"], axis=1), df["class"], test_size=0.2, random_state=42)
-
+def generate_categorical_dataset_model_wrapper(categorical_label_test=False):
     model = RandomForestClassifier(n_estimators=5, max_depth=2)
-    wrapper = CategoricalModelWrapper(model)
+    wrapper = CategoricalModelWrapper(model, categorical_label_test)
+    df = pd.read_csv(os.path.join(os.getcwd(), "tests\\test_assets\\data.csv"))
+    if categorical_label_test == True:
+        #Test for categorical label, convert titanic binary numeric label to categorical ["Survived"],["Not Survived"]
+        df["Survival"] = wrapper._perform_label_decoding(df["Survival"])
+    else:
+        #We only test NaN in categorical features and numerical target
+        df["Name"][0] = np.nan
+        df["Name"][10] = np.nan
+        df["Name"][20] = np.nan
+        df["Name"][30] = np.nan
+        df["Name"][40] = np.nan
+        df["Name"][50] = np.nan
+        df["Name"][60] = np.nan
+        df["Name"][70] = np.nan
+        df["Name"][80] = np.nan
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(["Survival"], axis=1), df["Survival"], test_size=0.2, random_state=42)
+
     X_train = wrapper._preprocessor(X_train)
-    y_train = wrapper._perform_label_encoding(y_train)
-    if categorical_label == False:
-        #We only test categorical features and numerical target
-        y_test = wrapper._perform_label_encoding(y_test)
+
+    if categorical_label_test == True:
+        y_train = wrapper._perform_label_encoding(y_train)
+        
     model.fit(X_train, y_train)
-    return CategoricalModelWrapper(model), X_test, y_test
+    return CategoricalModelWrapper(model, categorical_label_test), X_test, y_test
 
 def test_NaN_containing_categorical_dataset():
     _wrapper, _test_X, _test_y = generate_categorical_dataset_model_wrapper()
     explainer = ClassifierExplainer(
                     _wrapper, _test_X, _test_y)
     dashboard = ExplainerDashboard(explainer)
-    assert "NaN" in explainer.categorical_dict["buying"]
+    assert "NaN" in explainer.categorical_dict["Name"]
     
 def test_categorical_label():
     _wrapper, _test_X, _test_y = generate_categorical_dataset_model_wrapper(True)
     explainer = ClassifierExplainer(
                     _wrapper, _test_X, _test_y)
     dashboard = ExplainerDashboard(explainer)
-    assert "unacc" in explainer.labels
+    assert "Survived" in explainer.labels
