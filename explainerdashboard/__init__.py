7ceefb8479837e5db757f56f067c4aafd54bd22c
Oege Dijk <oegedijk@gmail.com>
2025-06-03 20:35:57 +0200

    Update __init__.py version 0.5.1

--- explainerdashboard/__init__.py
+++ explainerdashboard/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from .explainers import ClassifierExplainer, RegressionExplainer  # noqa
 from .dashboards import ExplainerDashboard, ExplainerHub, InlineExplainer  # noqa
